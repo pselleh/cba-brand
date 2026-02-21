db6c51fb02b6a84186e332bdf01d54aeb25b54bf
Phil Selleh <pselleh@gmail.com>
2026-02-21 22:54:00 +0000

    Fix brand structure to Paragon token format

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 module.exports = {
-  brand: {
-    colors: {
-      primary: "#0A3D62",
-      secondary: "#3C6382",
-      info: "#60A3BC",
-      success: "#78E08F",
-      warning: "#F6B93B",
-      danger: "#E55039"
-    }
+  colors: {
+    primary: { base: "#0A3D62" },
+    secondary: { base: "#3C6382" },
+    success: { base: "#78E08F" },
+    info: { base: "#60A3BC" },
+    warning: { base: "#F6B93B" },
+    danger: { base: "#E55039" }
   }
 };
